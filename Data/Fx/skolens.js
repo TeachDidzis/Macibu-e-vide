a9841b81aee48f9affd598ea4d07cb032407f8e9
TeachDidzis <didzis.abele@outlook.com>
2024-12-12 09:38:27 +0200

    Prog_vide_b1

--- Data/Fx/skolens.js
+++ Data/Fx/skolens.js
@@ -26,3 +26,33 @@ const clickableElements2 = document.querySelectorAll('.atgadeInfo');
     document.getElementById("bloks").style.display = "none"
     document.getElementById("bloks2").style.display = "block"
 })
+const textarea = document.getElementById("kodaVieta");
+
+textarea.addEventListener("keydown", function(event) {
+    if (event.key === "Tab") {
+        event.preventDefault(); // Prevent default tab behavior
+        const start = this.selectionStart;
+        const end = this.selectionEnd;
+
+        // Insert tab character at the cursor position
+        this.value = this.value.substring(0, start) + "\t" + this.value.substring(end);
+
+        // Move the cursor after the inserted tab
+        this.selectionStart = this.selectionEnd = start + 1;
+    }
+});
+
+document.getElementById("lejupielade").addEventListener("click", function(){
+    let datnesNosaukums = document.getElementById("nosaukums").value
+    let fails = document.createElement("a")
+    fails.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(document.getElementById("kodaVieta").value))
+    fails.setAttribute("download", datnesNosaukums)
+    fails.style.display = "none"
+    document.body.appendChild(fails)
+    fails.click()
+    document.body.removeChild(fails)
+})
+
+document.getElementById("palaist").addEventListener("click", function(){
+    document.getElementById("prieksskatijums").innerHTML = document.getElementById("kodaVieta").value
+})
